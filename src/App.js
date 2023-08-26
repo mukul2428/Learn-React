5c6f20d7f5ed34646f2e3a8a304faca2bdb78abd
Mukul Raghav <66879197+mukul2428@users.noreply.github.com>
2023-08-26 18:31:56 +0530

    api called using axios

--- src/App.js
+++ src/App.js
@@ -8,52 +8,36 @@ import StudentDispatchContext from "./context/StudentDispatchContext";
 //functional components
 //this App "A" should be in capital
 function App() {
-
-  // for forms
-  const contactDetails = [
-    {
-      id: "1",
-      school: "MAX Public",
-      place: "Mathura",
-      fname: "Sumit",
-      lname: "OP",
-    },
-    {
-      id: "2",
-      school: "Delhi Public",
-      place: "Agra",
-      fname: "Mukul",
-      lname: "OP",
-    },
-  ];
-
   const [editContact, setEditContact] = useState(null);
 
-  const [contact, dispatch] = useReducer(contactReducer, contactDetails);
+  const [contact, dispatch] = useReducer(contactReducer, []);
 
   // this function is used for setting state and it returns a state
   //instead of useState we can use this
   //it acts as a listner
   function contactReducer(contact, action) {
     switch (action.type) {
-      case 'ADD':
+      case "LOAD":
+        return action.payload;
+      case "ADD":
         return [
           ...contact,
           { ...action.payload, id: String(contact.length + 1) },
-        ]
-      case 'DELETE':
-        return contact.filter((data) => data.id !== action.payload)
-      case 'UPDATE':
-        const index = contact.findIndex((data) => data.id === action.payload.id);
+        ];
+      case "DELETE":
+        return contact.filter((data) => data.id !== action.payload);
+      case "UPDATE":
+        const index = contact.findIndex(
+          (data) => data.id === action.payload.id
+        );
         const newContact = [...contact];
-        newContact.splice(index, 1, action.payload)
+        newContact.splice(index, 1, action.payload);
         setEditContact(null); //setting edit usestate to its initialize state
         return newContact;
       default:
         return contact; // returning default state
     }
   }
-
 
   function handleEdit(id) {
     //finding id of that particular card and setting it to forms
@@ -64,12 +48,8 @@ function App() {
     <StudentContext.Provider value={contact}>
       <StudentDispatchContext.Provider value={dispatch}>
         <span className="app-container">
-          <Forms
-            editContact={editContact}
-          ></Forms>
-          <ShowFormData
-            handleEdit={handleEdit}
-          ></ShowFormData>
+          <Forms editContact={editContact}></Forms>
+          <ShowFormData handleEdit={handleEdit}></ShowFormData>
         </span>
       </StudentDispatchContext.Provider>
     </StudentContext.Provider>
--- src/components/ShowFormData.js
+++ src/components/ShowFormData.js
@@ -1,13 +1,26 @@
 import "./ShowFormData.css";
 import useStudentDispatch from "../hooks/StudentDispatch";
 import useStudent from "../hooks/Student";
+import axios from "axios";
+import { useEffect } from "react";
 
 function ShowFormData({ handleEdit }) {
+  const url = "https://my.api.mockaroo.com/student_contact.json?key=d6bb4330";
 
   // now we are using custom hooks instead of usecontext,
   // custom hook is internally using useContext
   const contact = useStudent();
   const dispatch = useStudentDispatch();
+
+  async function callApi() {
+    const res = await axios.get(url);
+    console.log(res.data);
+    dispatch({ type: "LOAD", payload: res.data });
+  }
+
+  useEffect(() => {
+    callApi();
+  }, []);
 
   return (
     <div className="main-box">
@@ -19,7 +32,7 @@ function ShowFormData({ handleEdit }) {
                 🖊️
               </span>
               <span
-                onClick={() => dispatch({ type: 'DELETE', payload: elem.id })}
+                onClick={() => dispatch({ type: "DELETE", payload: elem.id })}
                 className="delete-btn"
               >
                 🗑️
@@ -44,6 +57,7 @@ function ShowFormData({ handleEdit }) {
           </div>
         );
       })}
+      <button onClick={callApi}>Call API</button>
     </div>
   );
 }
