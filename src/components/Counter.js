8d41e0b5b1815a9ef796c5ea6b4e354d26c28ba1
Mukul Raghav <66879197+mukul2428@users.noreply.github.com>
2023-08-26 19:15:09 +0530

    useMemo & useCallback

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+
+// function fib(n) {
+//   if (n === 1 || n === 2) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 function Counter() {
   //state update do re-rendering
-  // we use useRef if want any value to be recorded but to be updated on screen
+  // we use useRef hook if want any value to be recorded but to be updated on screen
   // useRef is used to access dom apis
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
   //   let cnt = 0;
   let cnt = useRef(0);
   const inputRef = useRef(null);
+
+  //useMemo hook is used to save any value, saves calculation(caching)
+  //useCallback = whole function is memoized, function which will be made using this will not
+  // re-render again and again until its dependency or prop will not change
+  
+  // const fibMemorized = useMemo(() => {
+  //   return fib(count);
+  // }, [count]);
+
+  let fibMz = useCallback(function fib(n) {
+    if (n === 1 || n === 2) {
+      return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+  },[])
+
+  const fibMemorized = useMemo(() => {
+    return fibMz(count);
+  }, [count,fibMz]);
 
   let handleClick = () => {
     cnt.current++;
@@ -15,16 +41,16 @@ function Counter() {
     setCount(count + 1);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus();
-  },[])
+  }, []);
 
   return (
     <div>
-        <form action="">
-            <input ref={inputRef} type="text" placeholder="Enter Name" />
-        </form>
-      {count}
+      <form action="">
+        <input ref={inputRef} type="text" placeholder="Enter Name" />
+      </form>
+      {count} | {fibMemorized}
       <button onClick={handleClick}>Add</button>
     </div>
   );
